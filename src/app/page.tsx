8cd2550ac1ca8bcf16d211ab8de3f67d2fae3ad8
TonyV-UDEL <tveliu@udel.edu>
2025-09-30 16:34:37 -0400

    Added cool potato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         apiuwdhuaiwhdiuawndjnwka
         
+
+cool potato 
+
         <Image
           className="dark:invert"
           src="/next.svg"
