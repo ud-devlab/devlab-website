a9b8d9ed2998ec8e059ddf64afbb8a759e80a1a7
Michael Rothkopf <michaelhrothkopf@gmail.com>
2026-03-10 17:46:04 -0400

    skeleton structure for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,37 @@
+import { Link } from "react-router-dom";
 import { NavBar } from "../components/NavBar";
+import styles from "./Home.module.css";
 
 export function Home() {
     return (
         <div>
             <NavBar currentPage="home" />
-            <div className="content"></div>
+            <div className="content">
+                <div className={styles.mainBox}>
+                    <h1 className={styles.landingTitle}>DevLab</h1>
+                    <p className={styles.description}>Our club develops apps in a collaborative, beginner-friendly environment.</p>
+                </div>
+                <div className={styles.infoCards}>
+                    <Link to="/portfolio" className={styles.infoCard}>
+                        <div>
+                            <h3>Projects</h3>
+                            <p>See what we've been up to</p>
+                        </div>
+                    </Link>
+                    <Link to="/about" className={styles.infoCard}>
+                        <div>
+                            <h3>Leadership</h3>
+                            <p>Meet our team</p>
+                        </div>
+                    </Link>
+                    <Link to="/resources" className={styles.infoCard}>
+                        <div>
+                            <h3>Resources</h3>
+                            <p>Need something? It's probably here</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
